62cff7df23ec62b684c97fab519f5418cc0c312b
chatalrm <118279777+chatalrm@users.noreply.github.com>
2022-11-15 08:03:04 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDdapj3H3Dd2w9DHpLHwbzwpwSwU7rItpg",
-    authDomain: "chatrickfb.firebaseapp.com",
-    projectId: "chatrickfb",
-    storageBucket: "chatrickfb.appspot.com",
-    messagingSenderId: "815561540833",
-    appId: "1:815561540833:web:2cf0f52c625e8e281723a0",
-    measurementId: "G-5W02XQR53J"
+   apiKey: "AIzaSyDnUJQNwxP3ZV1k0Ooqwzf4pdoDPbUgH9I",
+  authDomain: "chatrmal43.firebaseapp.com",
+  projectId: "chatrmal43",
+  storageBucket: "chatrmal43.appspot.com",
+  messagingSenderId: "565091197451",
+  appId: "1:565091197451:web:c1666a9fc53c81492bdd0e",
+  measurementId: "G-5ZNKCB71X8"
     });
